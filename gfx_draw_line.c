fadfdeed32e9d1710f68c46eb939f10cbaf112c7
Epicurius <nneronin@student.hive.fi>
2021-07-25 14:24:00 +0300

    fixed gfx_draw_line by typecasting to ints

--- gfx_draw_line.c
+++ gfx_draw_line.c
@@ -6,29 +6,30 @@
 /*   By: nneronin <nneronin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/26 16:31:50 by nneronin          #+#    #+#             */
-/*   Updated: 2021/07/25 14:16:26 by nneronin         ###   ########.fr       */
+/*   Updated: 2021/07/25 14:22:53 by nneronin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libgfx.h"
 
-static inline int sign(float a, float b)
+static inline int	sign(float a, float b)
 {
 	if (a < b)
 		return (1);
 	return (-1);
 }
 
-static inline void line_calc(t_vector v1, t_vector v2, int *cath_x, int *cath_y)
+static inline void	line_calc(t_vector v1, t_vector v2,
+		int *cath_x, int *cath_y)
 {
-	if ((v2.y - v1.y) < 0)
-		*cath_x = (v2.y - v1.y) * -1;
+	if (((int)v2.y - (int)v1.y) < 0)
+		*cath_x = ((int)v2.y - (int)v1.y) * -1;
 	else
-		*cath_x = (v2.y - v1.y);
-	if ((v2.x - v1.x) < 0)
-		*cath_y = (v2.x - v1.x) * -1;
+		*cath_x = ((int)v2.y - (int)v1.y);
+	if (((int)v2.x - (int)v1.x) < 0)
+		*cath_y = ((int)v2.x - (int)v1.x) * -1;
 	else
-		*cath_y = (v2.x - v1.x);
+		*cath_y = ((int)v2.x - (int)v1.x);
 }
 
 void	gfx_draw_line(SDL_Surface *surf, Uint32 color, t_vector v1, t_vector v2)
@@ -40,9 +41,9 @@ void	gfx_draw_line(SDL_Surface *surf, Uint32 color, t_vector v1, t_vector v2)
 
 	line_calc(v1, v2, &cath_x, &cath_y);
 	overflow_y = cath_y - cath_x;
-	while (v1.x != v2.x || v1.y != v2.y)
+	while ((int)v1.x != (int)v2.x || (int)v1.y != (int)v2.y)
 	{
-		set_pixel(surf, v2.x, v2.y, color);
+		set_pixel(surf, (int)v1.x, (int)v1.y, color);
 		overflow_x = overflow_y * 2;
 		if (overflow_x > -(cath_x))
 		{
